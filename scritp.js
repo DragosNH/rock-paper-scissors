5878d62df4664b986aab6b1dc1bc9a77bc0d71ce
Dragoş <edragos33@gmail.com>
2025-09-29 16:14:00 +0200

    added the rules

--- scritp.js
+++ scritp.js
@@ -4,23 +4,56 @@ const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
 const gameArray = ["Rock", "Paper", "Scissors"];
-let randomValue = Math.floor(Math.random() * 3);
+let randomValue = Math.floor(Math.random() * gameArray.length);
+const computer = gameArray[randomValue];
 
+const game_play = () => {
+    if (computer == "Rock" && rock) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nTie`;
+    }
+    if (computer == "Paper" && rock) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nComputer wins!`;
+    }
+    if (computer == "Scissors" && rock) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nYou Win!`;
+    }
 
-gameScreen.innerText = `Computer choosed: ${gameArray[randomValue]}`;
+    if (computer == "Rock" && paper) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nYou Win!`;
+    }
+    if (computer == "Paper" && paper) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nTie`;
 
-const game_play = () =>{
+    }
+    if (computer == "Scissors" && paper) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nComputer wins!`;
+    }
+
+    if (computer == "Rock" && scissors) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nComputer wins!`;
+    }
+    if (computer == "Paper" && scissors) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nYou Win!`;
+    }
+    if (computer == "Scissors" && scissors) {
+        gameScreen.innerText = `Computer choosed: ${computer} \n\nTie`;
+    }
 
 }
 
 rock.addEventListener("click", () => {
     gameArray[0];
+    game_play()
+
 })
 
 paper.addEventListener("click", () => {
     gameArray[1];
+    game_play();
+
 })
 
 scissors.addEventListener("click", () => {
     gameArray[2];
+    game_play()
 })
