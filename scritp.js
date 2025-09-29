2eabc6838b55c5285794a4a392be260d09afdbc4
Dragoş <edragos33@gmail.com>
2025-09-29 15:37:46 +0200

    added random value generator

--- scritp.js
+++ scritp.js
@@ -2,3 +2,8 @@ const gameScreen = document.querySelector(".game-screen");
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
+
+const gameArray = ["Rock", "Paper", "Scissors"];
+let randomValue = Math.floor(Math.random() * 3);
+
+gameScreen.innerText = gameArray[randomValue];
