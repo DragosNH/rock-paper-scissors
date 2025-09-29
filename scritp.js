23f8f38105643a25345104fb54da6a7aa93bce2d
Dragoş <edragos33@gmail.com>
2025-09-29 15:32:19 +0200

    declaerd the variables

---
+++ scritp.js
@@ -0,0 +1,4 @@
+const gameScreen = document.querySelector(".game-screen");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
